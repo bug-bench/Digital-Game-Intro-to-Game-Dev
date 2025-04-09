cf0c4712dd893ca94e26a636291c6a3152890d6e
Pac-ManLikeGameCreator <amitesh.d.amiteshsharma@student.uts.edu.au>
2025-04-09 10:27:41 +1000

    Pogo ability first edition
    Changes Required - Making pogoable objects not jumpable

---
+++ Assets/Scripts/Player/Pogo Ability.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PogoAbility : MonoBehaviour
+{
+    [Header("Pogo Settings")]
+    public float pogoBounceForce = 15f;
+    public float pogoCheckDistance = 0.5f;
+
+    private Rigidbody2D rb;
+    private bool canPogo = false;
+    private Transform playerTransform;
+    private LayerMask pogoLayer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerTransform = transform;
+
+        pogoLayer = LayerMask.GetMask("PogoLayer");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && canPogo)
+        {
+            DoPogoBounce();
+        }
+
+        CheckForPogoable();
+    }
+
+    void CheckForPogoable()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(playerTransform.position, Vector2.down, pogoCheckDistance, pogoLayer);
+        Debug.DrawRay(playerTransform.position, Vector2.down * pogoCheckDistance, Color.red);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Pogoable object detected: " + hit.collider.gameObject.name);
+            canPogo = true;
+        }
+        else
+        {
+            canPogo = false;
+        }
+    }
+
+    void DoPogoBounce()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, pogoBounceForce);
+        canPogo = false;
+
+        //Feedback
+        Debug.Log("POGO activated!");
+    }
+}
