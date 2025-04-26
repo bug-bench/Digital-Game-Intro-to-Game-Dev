526b8f81138d3bf30464609c403699d34f63b259
Marcy P <rorypalmer.rp@gmail.com>
2025-04-27 01:24:14 +1000

    Working camera / level system

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -31,4 +31,18 @@ public class CameraFollow : MonoBehaviour
         minY = bounds.minY;
         maxY = bounds.maxY;
     }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Vector3 topLeft = new Vector3(minX, maxY, 0);
+        Vector3 topRight = new Vector3(maxX, maxY, 0);
+        Vector3 bottomLeft = new Vector3(minX, minY, 0);
+        Vector3 bottomRight = new Vector3(maxX, minY, 0);
+
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomRight, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, topLeft);
+    }
 }
